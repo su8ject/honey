408e47f293663066346b6e4d0eee3b1564a2dddb
su8ject <krivizyuk.andrei@gmail.com>
2025-12-17 21:44:27 +0200

    Підключення до MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const PORT = 5000;
+const DB_URL = "mongodb+srv://krivizyukandrei_db_user:uKmN1XVjDTwqaFv1@cluster0.932gsdh.mongodb.net/?appName=Cluster0";
 
 const app = express();
 
@@ -18,4 +20,13 @@ app.post("/", (req, res) => {
     res.status(200).send("Data received");
 });
 
-app.listen(PORT, () => console.log("Server started on port " + PORT));
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log("Server started on port " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+startApp();
