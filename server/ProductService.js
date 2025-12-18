6e2a52b25fd0601dc097f903c7fe3791d1f7def3
su8ject <krivizyuk.andrei@gmail.com>
2025-12-18 19:40:45 +0200

    Створив сервіс для продуктів (незавершено)

---
+++ server/ProductService.js
@@ -0,0 +1,59 @@
+import Product from "./Product.js";
+
+class ProductService {
+    async create(post) {
+        const createdProduct = await Product.create(post);
+        return createdProduct;
+    };
+
+    async getAll(req, res) {
+        try {
+            const products = await Product.find();
+            return res.json(products);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+
+    };
+
+    async getOne(req, res) {
+        try {
+            const {id} = req.params;
+            if (!id) {
+                return res.status(400).json({message: "ID не вказано"});
+            }
+            const product = await Product.findById(id);
+            return res.json(product);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+    };
+
+    async update(req, res) {
+        try {
+            const product = req.body;
+            if (!product._id) {
+                return res.status(400).json({message: "ID не вказано"});
+            }
+            const updatedProduct = await Product.findByIdAndUpdate(product._id, product, {new: true});
+            return res.json(updatedProduct);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+    };
+
+    async delete(req, res) {
+        try {
+            const {id} = req.params;
+            if (!id) {
+                return res.status(400).json({message: "ID не вказано"});
+            }
+            const product = await Product.findByIdAndDelete(id);
+            return res.json(product);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+    };
+};
+
+export default new ProductService();
