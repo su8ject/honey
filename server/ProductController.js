ae21246939947aa7e66461fdbdf73e9b736291ca
su8ject <krivizyuk.andrei@gmail.com>
2025-12-18 19:40:22 +0200

    Створив Контроллер для продуктів

---
+++ server/ProductController.js
@@ -0,0 +1,64 @@
+import Product from "./Product.js";
+import ProductService from "./ProductService.js";
+
+class PostController {
+    async create(req, res) {
+        try {
+            const product = await ProductService.create(req.body);
+            res.json(product);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+    };
+
+    async getAll(req, res) {
+        try {
+            const products = await Product.find();
+            return res.json(products);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+
+    };
+
+    async getOne(req, res) {
+        try {
+            const {id} = req.params;
+            if (!id) {
+                return res.status(400).json({message: "ID не вказано"});
+            }
+            const product = await Product.findById(id);
+            return res.json(product);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+    };
+
+    async update(req, res) {
+        try {
+            const product = req.body;
+            if (!product._id) {
+                return res.status(400).json({message: "ID не вказано"});
+            }
+            const updatedProduct = await Product.findByIdAndUpdate(product._id, product, {new: true});
+            return res.json(updatedProduct);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+    };
+
+    async delete(req, res) {
+        try {
+            const {id} = req.params;
+            if (!id) {
+                return res.status(400).json({message: "ID не вказано"});
+            }
+            const product = await Product.findByIdAndDelete(id);
+            return res.json(product);
+        } catch (e) {
+            res.status(500).json(e);
+        };
+    };
+};
+
+export default new PostController();
