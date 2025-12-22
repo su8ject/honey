c17e38f2cd5c85d3fb7f36006489ac1a53c3adb3
su8ject <74069947+su8ject@users.noreply.github.com>
2025-12-22 16:52:19 +0200

    запит delete

--- server/ProductController.js
+++ server/ProductController.js
@@ -41,12 +41,8 @@ class PostController {
 
     async delete(req, res) {
         try {
-            const {id} = req.params;
-            if (!id) {
-                return res.status(400).json({message: "ID не вказано"});
-            }
-            const product = await Product.findByIdAndDelete(id);
-            return res.json(product);
+            const product = await ProductService.delete(req.params.id);
+            return product;
         } catch (e) {
             res.status(500).json(e);
         };
--- server/ProductService.js
+++ server/ProductService.js
@@ -27,17 +27,12 @@ class ProductService {
         return updatedProduct;
     };
 
-    async delete(req, res) {
-        try {
-            const {id} = req.params;
-            if (!id) {
-                return res.status(400).json({message: "ID не вказано"});
-            }
-            const product = await Product.findByIdAndDelete(id);
-            return res.json(product);
-        } catch (e) {
-            res.status(500).json(e);
-        };
+    async delete(id) {
+        if (!id) {
+            throw new Error("ID не вказано");
+        }
+        const product = await Product.findByIdAndDelete(id);
+        return product;
     };
 };
 
