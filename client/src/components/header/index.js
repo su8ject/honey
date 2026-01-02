a88112a84a8bae7abb1588dce3e52e0deebcc1ba
su8ject <74069947+su8ject@users.noreply.github.com>
2026-01-02 16:11:11 +0200

    Переписав header (не стилізовано)

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,19 +1,129 @@
 import { useState } from "react";
 import { Logo } from "./logo";
-import { Menu } from "./menu";
-import { Navigation } from "./navigation";
 import "./style.scss";
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
-  const [isMenuActive, setIsMenuActive] = useState(false);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const pages = [{name: "Головна", link: "/"}, {name: "Продукти пасічництва", link: "products-honey"}, {name: "Реманент пасівництва", link: "products-remanent"}, {name: "Доставка", link: "delivery"}];
+
+  const handleOpenNavMenu = (e) => {
+    setAnchorElNav(e.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <div className="header">
-      <Menu isMenuActive={isMenuActive} setIsMenuActive={setIsMenuActive} />
-      <Logo />
-      <Navigation />
-      <a href={"tel:+380989656054"} className="phone">
-        +380989656054
-      </a>
-    </div>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Logo sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component={NavLink}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem component={NavLink} to={page.link} key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>
+                    {page.name}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Logo sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component={NavLink}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                component={NavLink}
+                to={page.link}
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton size="large" aria-label="show cart items" color="inherit">
+              <ShoppingCartOutlinedIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
--- client/src/components/header/logo.js
+++ client/src/components/header/logo.js
@@ -1,10 +1,10 @@
 import { Svg } from "../svg";
+import SvgIcon from "@mui/material/SvgIcon";
 
-export const Logo = () => {
+export const Logo = (props) => {
   return (
-    <div className="header--logo">
-      <Svg hash={"logo"} className="logo--img" />
-      <span className="header--logo-text">MykolaHoney</span>
-    </div>
+    <SvgIcon {...props}>
+      <Svg  hash={"logo"} className="logo--img" />
+    </SvgIcon>
   );
 };
