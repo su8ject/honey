ec6394250684029562d43170d4817fb73bfffc96
su8ject <74069947+su8ject@users.noreply.github.com>
2026-04-22 14:47:17 +0300

    Додав підзаголовок

---
+++ client/src/components/UI/subTitle.js
@@ -0,0 +1,9 @@
+import Typography from "@mui/material/Typography";
+
+export const SubTitle = ({text}) => {
+    return (
+        <Typography variant="h3" margin="10px">
+            {text}
+        </Typography>
+    );
+};
