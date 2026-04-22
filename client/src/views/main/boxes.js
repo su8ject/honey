27071e7f21f3ebb81473ffd91f21900d16014940
su8ject <74069947+su8ject@users.noreply.github.com>
2026-04-22 14:48:17 +0300

    Змінив картки на головній сторінці

--- client/src/views/main/boxes.js
+++ client/src/views/main/boxes.js
@@ -1,15 +1,23 @@
-import { Title } from "../../components/UI/title";
-import { StyledBox } from "./box";
-import List from "@mui/material/List";
+import Box from "@mui/material/Box";
+import { MainCard } from "./mainCard";
+import { SubTitle } from "../../components/UI/subTitle";
 
 export const Boxes = ({ type, array, header }) => {
   
   return (
-    <List>
-      <Title text={header}/>
-      {array.map((item) => (
-        <StyledBox item={item} type={type} key={item.id} />
-      ))}
-    </List>
+    <Box textAlign="center">
+      <SubTitle text={header}/>
+      <Box
+        sx={{
+          width: "100%",
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(min(200px, 100%), 1fr))",
+          gap: 2,
+        }}>
+        {array.map((item) => (
+          <MainCard item={item} type={type} key={item.id} />
+        ))}
+        </Box>
+    </Box>
   );
 };
---
+++ client/src/views/main/mainCard.js
@@ -0,0 +1,63 @@
+import { Svg } from "../../components/svg";
+import { boxType } from "../../static";
+import { StyledSvg } from "../../components/UI/styledSvg";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+
+export const MainCard = ({ type, item }) => {
+  if (type === boxType.advantages) {
+    return (
+      <Card>
+        <CardContent sx={{ padding: "10px" }}>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}>
+            <StyledSvg hash={item.svgHash}/>
+            <Typography variant="h5">
+              {item.header}
+            </Typography>
+          </Box>
+          <Typography variant="body2" sx={{color: "text.secondary"}}>
+            {item.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  } else {
+    return (
+      <Card sx={{ display: "flex" }}>
+        <CardContent
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: 1,
+            padding: "10px" 
+          }}>
+          <Typography variant="body2" sx={{color: "text.secondary"}}>
+            {item.text}
+          </Typography>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              mt: "auto",
+            }}>
+            <StyledSvg hash="chat"/>
+            <Typography variant="h5">
+              {item.header}
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
+    );
+  }
+};
