c8d485532c15ec95c0cf93c42710b76b510100c9
su8ject <74069947+su8ject@users.noreply.github.com>
2026-04-22 22:26:40 +0300

    Змінив Title

--- client/src/components/UI/subTitle.js
+++ client/src/components/UI/subTitle.js
@@ -2,7 +2,7 @@ import Typography from "@mui/material/Typography";
 
 export const SubTitle = ({ text }) => {
   return (
-    <Typography variant="h3" margin="10px">
+    <Typography textAlign="center" variant="h3" margin="10px">
       {text}
     </Typography>
   );
