86077d69548c33a3d59983fb152d3db9a050ff3a
su8ject <74069947+su8ject@users.noreply.github.com>
2026-04-22 22:13:29 +0300

    Форматував

--- client/src/components/UI/title.js
+++ client/src/components/UI/title.js
@@ -1,9 +1,9 @@
 import Typography from "@mui/material/Typography";
 
 export const Title = ({ text }) => {
-    return (
-        <Typography variant="h2" component="h2">
-            {text}
-        </Typography>
-    );
+  return (
+    <Typography variant="h2" component="h2">
+      {text}
+    </Typography>
+  );
 };
