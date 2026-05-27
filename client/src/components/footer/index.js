8bbdaa6dc90473e249cb34297e27459e69f33310
su8ject <74069947+su8ject@users.noreply.github.com>
2026-05-27 11:27:35 +0300

    Намагання зробити футер

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,11 +1,29 @@
 import { Svg } from "../svg";
-import "./style.scss";
 import { StyledSvg } from "../UI/styledSvg";
+import {pages} from "../header/pages"
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { NavLink } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer--wrapper">
+    <Box>
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item>
+            <Typography variant="h6">Про нас</Typography>
+            <Typography variant="body2">
+              Сімейна пасіка у селі Високе.
+            </Typography>
+          </Grid>
+          <Grid item>
+            {pages.map((page) => (
+              <NavLink to={page.link} key={page}/>
+            ))}
+          </Grid>
+        </Grid>
         <StyledSvg hash="logo" sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
         <a href={"tel:+380989656054"} className="phone none-decoration">
           +380989656054
@@ -19,7 +37,7 @@ export const Footer = () => {
         <a target="_blank" href="https://www.instagram.com/mukola_m_/?hl=ru">
           <Svg hash={"inst"} className="footer--social-icon" />
         </a>
-      </div>
-    </div>
+      </Container>
+    </Box>
   );
 };
